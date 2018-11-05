5736004109db4e509566b0c51e05e9f6b2f37f06
Alexey <alexeyorc@gmail.com>
2018-11-05 12:14:37 +0200

    Code refactoring and add comments in EditorListener.

--- web-author-cmis-plugin/src/main/java/com/oxygenxml/cmis/web/EditorListener.java
+++ web-author-cmis-plugin/src/main/java/com/oxygenxml/cmis/web/EditorListener.java
@@ -29,15 +29,41 @@ import ro.sync.exml.workspace.api.editor.ReadOnlyReason;
 import ro.sync.exml.workspace.api.options.WSOptionsStorage;
 import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
 
+
+enum Options {
+	NON_VERSIONABLE("nonversionable"),
+	IS_CHECKED_OUT("checkedout"),
+	NO_SUPPORT("nosupportfor"),
+	OLD_VERSION("oldversion"),
+	TO_BLOCK("block");
+	
+	
+	private final String value;
+	
+	
+	Options(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+}
+
+
 public class EditorListener implements WorkspaceAccessPluginExtension {
 
 	private static final Logger logger = Logger.getLogger(EditorListener.class.getName());
 
-	private static final String NON_VERSIONABLE 	  = "nonversionable";
-	private static final String IS_CHECKED_OUT        = "checkedout";
-	private static final String TO_BLOCK              = "block";
-	private static final String NO_SUPPORT			  = "nosupportfor";
+	private AuthorAccess authorAccess;
+	private PluginResourceBundle rb;
 
+	private URL url;
+
+	private String urlWithoutContextId;
+	private UserCredentials credentials;
+	private CmisURLConnection connection;
+	
 	@Override
 	public void applicationStarted(StandalonePluginWorkspace pluginWorkspaceAccess) {
 
@@ -59,73 +85,52 @@ public class EditorListener implements WorkspaceAccessPluginExtension {
 	 */
 	@VisibleForTesting
 	public void editingSessionStarted(WebappPluginWorkspace webappPluginWorkspace, AuthorDocumentModel documentModel) {
-		AuthorAccess authorAccess = documentModel.getAuthorAccess();
+		authorAccess = documentModel.getAuthorAccess();
 		authorAccess.getWorkspaceAccess();
 
 		SessionStore sessionStore = webappPluginWorkspace.getSessionStore();
 
 		// Get URL and ContextID for CmisURLConnection
-		URL url = authorAccess.getEditorAccess().getEditorLocation();
+		url = authorAccess.getEditorAccess().getEditorLocation();
 		
 		if (url == null || !url.getProtocol().equals("cmis")) {
 		  return;
 		}
 		
-		String urlWithoutContextId = null;
 		urlWithoutContextId = url.getProtocol() + "://" + url.getHost() + url.getPath();
 		
 		String contextId = url.getUserInfo();
-		UserCredentials credentials = sessionStore.get(contextId, "wa-cmis-plugin-credentials");
-		CmisURLConnection connection = new CmisURLConnection(url, new CMISAccess(), credentials);
+		credentials = sessionStore.get(contextId, "wa-cmis-plugin-credentials");
+		
+		connection = new CmisURLConnection(url, new CMISAccess(), credentials);
 
-		PluginResourceBundle rb = webappPluginWorkspace.getResourceBundle();
+		rb = webappPluginWorkspace.getResourceBundle();
 
 		try {
-			logger.info("EditorListener is loaded!");
+			logger.info("EditorListener was loaded!");
 
-			if (url.getQuery() != null && url.getQuery().contains(CmisActions.OLD_VERSION)) {
-				String objectId = getOlderVersionDocumentId(url);
-				Document oldDoc = (Document) connection.getResourceController(urlWithoutContextId)
-									.getCmisObj(objectId);
-
-				String language = webappPluginWorkspace.getUserInterfaceLanguage();
-				SimpleDateFormat df = new SimpleDateFormat("d MMM yyyy HH:mm:ss", new Locale(language));
+			if (url.getQuery() != null && url.getQuery().contains(Options.OLD_VERSION.getValue())) {
 				
-				Date lastMod = oldDoc.getLastModificationDate().getTime();
-				
-				documentModel.getAuthorAccess().getEditorAccess().setReadOnly(
-						new ReadOnlyReason(rb.getMessage(TranslationTags.OLD_VER_WARNING) + " : " + df.format(lastMod)));
-				documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
-						.setPseudoClass(TO_BLOCK);
-				
-				if(oldDoc.isPrivateWorkingCopy() == null || oldDoc.getCheckinComment() == null) {
-					documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
-					.setPseudoClass(NO_SUPPORT);
-				}
+				setOldVersionsOptions(webappPluginWorkspace, documentModel);
 
 			} else {
 				Document document = (Document) connection.getCMISObject(urlWithoutContextId);
 
+				// If server doesn't support private working copy and check in comments features
+				// we disable this actions in editor.
 				if(document.isPrivateWorkingCopy() == null || document.getCheckinComment() == null) {
 					documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
-					.setPseudoClass(NO_SUPPORT);
+					.setPseudoClass(Options.NO_SUPPORT.getValue());
 				}
 				
+				// If server doesn't support version control system
+				// we disable this feature in editor.
 				if (!document.isVersionable()) {
 					documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
-							.setPseudoClass(NON_VERSIONABLE);
+							.setPseudoClass(Options.NON_VERSIONABLE.getValue());
 				} else if (document.isVersionSeriesCheckedOut()) {
-					String versionSeriesCheckedOutBy = document.getVersionSeriesCheckedOutBy();
-
-					if (!credentials.getUsername().equals(versionSeriesCheckedOutBy)) {
-						documentModel.getAuthorAccess().getEditorAccess().setReadOnly(new ReadOnlyReason(
-								rb.getMessage(TranslationTags.CHECKED_OUT_BY) + " " + versionSeriesCheckedOutBy + "."));
-						documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
-								.setPseudoClass(TO_BLOCK);
-					} else {
-						documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
-								.setPseudoClass(IS_CHECKED_OUT);
-					}
+					
+					setVersionableOptions(documentModel, document);
 
 				} else {
 					if (isCheckOutRequired() && !document.isVersionSeriesCheckedOut()) {
@@ -139,13 +144,76 @@ public class EditorListener implements WorkspaceAccessPluginExtension {
 			logger.info(e1.getMessage());
 		}
 	}
+
+	/**
+	 * Set Web Author's editor to versionable document options.
+	 * 
+	 * @param documentModel
+	 * @param credentials
+	 * @param rb
+	 * @param document
+	 */
+	private void setVersionableOptions(AuthorDocumentModel documentModel, Document document) {
+		String versionSeriesCheckedOutBy = document.getVersionSeriesCheckedOutBy();
+
+		if (!credentials.getUsername().equals(versionSeriesCheckedOutBy)) {
+			
+			documentModel.getAuthorAccess().getEditorAccess().setReadOnly(new ReadOnlyReason(
+					rb.getMessage(TranslationTags.CHECKED_OUT_BY) + " " + versionSeriesCheckedOutBy + "."));
+			documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
+					.setPseudoClass(Options.TO_BLOCK.getValue());
+			
+		} else {
+			
+			documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
+					.setPseudoClass(Options.IS_CHECKED_OUT.getValue());
+			
+		}
+	}
+
+	/**
+	 * Set Web Author's editor to older version 
+	 * of document options.
+	 * 
+	 * @param webappPluginWorkspace
+	 * @param documentModel
+	 * @param url
+	 * @param urlWithoutContextId
+	 * @param connection
+	 * @param rb
+	 * @throws MalformedURLException
+	 */
+	private void setOldVersionsOptions(WebappPluginWorkspace webappPluginWorkspace, AuthorDocumentModel documentModel)
+			throws MalformedURLException {
+		
+		String objectId = getOldVersionDocumentId(url);
+		Document oldDoc = (Document) connection.getResourceController(urlWithoutContextId)
+							.getCmisObj(objectId);
+
+		String language = webappPluginWorkspace.getUserInterfaceLanguage();
+		SimpleDateFormat df = new SimpleDateFormat("d MMM yyyy HH:mm:ss", new Locale(language));
+		
+		Date lastMod = oldDoc.getLastModificationDate().getTime();
+		
+		documentModel.getAuthorAccess().getEditorAccess().setReadOnly(
+				new ReadOnlyReason(rb.getMessage(TranslationTags.OLD_VER_WARNING) + " : " + df.format(lastMod)));
+		documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
+				.setPseudoClass(Options.TO_BLOCK.getValue());
+		
+		if(oldDoc.isPrivateWorkingCopy() == null || oldDoc.getCheckinComment() == null) {
+			documentModel.getAuthorDocumentController().getAuthorDocumentNode().getRootElement()
+			.setPseudoClass(Options.NO_SUPPORT.getValue());
+			
+		}
+	}
 	
 	/**
+	 * Get ObjectId from query part of our custom url.
 	 * 
 	 * @param url
 	 * @return objectId of old version of document.
 	 */
-	private String getOlderVersionDocumentId(URL url) {
+	private String getOldVersionDocumentId(URL url) {
 		HashMap<String, String> queryPart = new HashMap<>();
 		
 		for(String pair : url.getQuery().split("&")) {
@@ -157,7 +225,11 @@ public class EditorListener implements WorkspaceAccessPluginExtension {
 		
 		return objectId;
 	}
-
+	
+	/**
+	 * 
+	 * @return true if checked out is required
+	 */
 	public static boolean isCheckOutRequired() {
 		WSOptionsStorage optionsStorage = PluginWorkspaceProvider.getPluginWorkspace().getOptionsStorage();
 		String optionValue = optionsStorage.getOption(CmisPluginConfigExtension.CHECKOUT_REQUIRED, "on");
