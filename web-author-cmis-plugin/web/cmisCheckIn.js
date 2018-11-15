1b4255e00b5b34dbb6b077b3522a936ee1d28445
andreip1123 <andrei.popa1123@gmail.com>
2018-11-15 18:51:05 +0200

    WA-2422: Trigger a save before check in, show warning if that fails. Fix callback. Some refactoring on check in dialog.

--- web-author-cmis-plugin/web/cmisCheckIn.js
+++ web-author-cmis-plugin/web/cmisCheckIn.js
@@ -14,82 +14,65 @@ CmisCheckInAction.prototype.getSmallIcon = function(devicePixelRation) {
 };
 
 CmisCheckInAction.prototype.isEnabled = function() {
-    var isEnabled = false;
-    if (cmisStatus && cmisStatus !== 'checkedout') {
-        isEnabled = true;
-    }
-    return isEnabled;
+    return window.cmisStatus && window.cmisStatus !== 'checkedout';
+};
+
+CmisCheckInAction.prototype.actionPerformedInternal_ = function (callback) {
+  if (this.editor.isDirty()) {
+    // Should only happen if save action failed.
+    this.editor.problemReporter.showWarning(tr(msgs.SAVE_CHANGES_BEFORE_CHECK_IN_));
+    return;
+  }
+  var noSupport = document.querySelector('[data-root="true"]').getAttribute('data-pseudoclass-nosupportfor');
+
+  this.dialog = workspace.createDialog();
+  this.dialog.setTitle(tr(msgs.CMIS_CHECK_IN));
+  var dialogElement = this.dialog.getElement();
+  dialogElement.innerHTML = '';
+
+  if (noSupport !== 'true') {
+    goog.dom.append(dialogElement, createCommitMessageElements());
+    this.dialog.setPreferredSize(300, 350);
+  } else {
+    this.dialog.setPreferredSize(250, 180);
+  }
+
+  dialogElement.appendChild(createVersionForm());
+
+  this.dialog.show();
+
+  var editor = this.editor;
+  this.dialog.onSelect(goog.bind(function(key, e) {
+      if (key === 'ok') {
+        // If commit message is not supported, the textarea is not present.
+        var commitMessageTextarea = dialogElement.querySelector('#cmis-commit-message');
+        var commitMessage = commitMessageTextarea ? commitMessageTextarea.value.replace(/["']/g, "") : null;
+
+        // Get the selected version type.
+        var selectedVersionRadio = document.querySelector('.cmis-version-label input[type="radio"]:checked');
+        editor.getActionsManager().invokeOperation(
+          'com.oxygenxml.cmis.web.action.CmisCheckIn', {
+            action: 'cmisCheckin',
+            commit: commitMessage,
+            state: selectedVersionRadio ? selectedVersionRadio.value : ''
+          }, callback);
+
+        window.cmisStatus = false;
+      } else {
+        goog.isFunction(callback) && callback()
+      }
+    },
+    this));
 };
 
 CmisCheckInAction.prototype.actionPerformed = function(callback) {
-    // Do not allow check in while the document is dirty.
+    // Save document immediately if it is dirty.
     if (this.editor.isDirty()) {
-      this.editor.problemReporter.showWarning(tr(msgs.SAVE_CHANGES_BEFORE_CHECK_IN_));
-      return;
-    }
-    var root = document.querySelector('[data-root="true"]');
-    var noSupport = root.getAttribute('data-pseudoclass-nosupportfor');
-
-    this.dialog = workspace.createDialog();
-    this.dialog.setTitle(tr(msgs.CMIS_CHECK_IN));
-    var dialogElement = this.dialog.getElement();
-
-    if (noSupport !== 'true') {
-        dialogElement.innerHTML = '';
-        var checkInMessageSpan = document.createElement('span');
-        checkInMessageSpan.textContent = tr(msgs.CHECK_IN_MESSAGE_);
-        dialogElement.appendChild(checkInMessageSpan);
-        dialogElement.appendChild(document.createElement('br'));
-        this.dialog.setPreferredSize(300, 350);
-
-        var input = document.createElement('textarea');
-
-        input.setAttribute('style', 'margin:0px;width:255px;height:125px;resize:none;');
-        input.setAttribute('type', 'text');
-        input.setAttribute('id', 'input');
-
-        dialogElement.appendChild(input);
-
+      var saveAction = this.editor.getActionsManager().getActionById('Author/Save');
+      saveAction.actionPerformed(goog.bind(this.actionPerformedInternal_, this, callback));
     } else {
-        this.dialog.setPreferredSize(250, 180);
+      this.actionPerformedInternal_(callback);
     }
-
-    dialogElement.appendChild(createVersionForm());
-
-    this.dialog.show();
-
-    var editor = this.editor;
-    this.dialog.onSelect(goog.bind(function(key, e) {
-            if (key === 'ok') {
-                var root = document.querySelector('[data-root="true"]');
-                var noSupport = root.getAttribute('data-pseudoclass-nosupportfor');
-
-                var commitMessage;
-                var verstate;
-
-                if (noSupport !== 'true') {
-                    commitMessage = dialogElement.querySelector('#input').value.replace(/["']/g, "");
-                }
-
-                if (dialogElement.querySelector('#radio1').checked) {
-                    verstate = dialogElement.querySelector('#radio1').value;
-                } else if (dialogElement.querySelector('#radio2').checked) {
-                    verstate = dialogElement.querySelector('#radio2').value;
-                }
-
-                editor.getActionsManager().invokeOperation(
-                    'com.oxygenxml.cmis.web.action.CmisCheckIn', {
-                        action: 'cmisCheckin',
-                        commit: commitMessage,
-                        state: verstate
-                    }, callback);
-
-                cmisStatus = false;
-            }
-
-            this.dialog.dispose();
-        },
-        this));
 };
 
 /**
@@ -98,30 +81,33 @@ CmisCheckInAction.prototype.actionPerformed = function(callback) {
  */
 function createVersionForm() {
     var createDom = goog.dom.createDom;
-
-    var radio1 = createDom('input', {
-      id: 'radio1',
-      type: 'radio',
-      name: 'state',
-      value: 'major'
-    });
-    radio1.setAttribute('checked', '');
-
-    var radio2 = createDom('input', {
-      id: 'radio2',
-      type: 'radio',
-      name: 'state',
-      value: 'minor'
-    });
+    var majorVersionRadio = createDom('input', { type: 'radio', name: 'state', value: 'major' });
+    majorVersionRadio.setAttribute('checked', '');
+    var minorVersionRadio = createDom('input', { type: 'radio', name: 'state', value: 'minor' });
 
     return createDom('form', '',
       createDom('label', 'cmis-version-label',
-        radio1,
+        majorVersionRadio,
         tr(msgs.MAJOR_VERSION_)
       ),
       createDom('label', 'cmis-version-label',
-        radio2,
+        minorVersionRadio,
         tr(msgs.MINOR_VERSION_)
       )
     );
+}
+
+/**
+ * Create the commit message textarea.
+ * @returns Array<Element> A list of commit message related elements to be appended to the dialog element.
+ */
+function createCommitMessageElements() {
+  return [
+    goog.dom.createDom('div', '', tr(msgs.CHECK_IN_MESSAGE_)),
+    goog.dom.createDom('textarea', {
+      id: 'cmis-commit-message',
+      style: 'margin:0px; width:255px; height:125px; resize:none;',
+      type: 'text'
+    })
+  ]
 }
